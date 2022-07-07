e33fc04225462968d69d97979bf9eeb91cfa728d
nriedel <nriedel@serranosystems.com>
2022-07-07 13:06:47 -0700

    Add eclipse-generated uzlib makefiles

---
+++ CortexM33/src/subdir.mk
@@ -0,0 +1,42 @@
+################################################################################
+# Automatically-generated file. Do not edit!
+################################################################################
+
+# Add inputs and outputs from these tool invocations to the build variables 
+C_SRCS += \
+../src/adler32.c \
+../src/crc32.c \
+../src/defl_static.c \
+../src/genlz77.c \
+../src/tinfgzip.c \
+../src/tinflate.c \
+../src/tinfzlib.c 
+
+OBJS += \
+./src/adler32.o \
+./src/crc32.o \
+./src/defl_static.o \
+./src/genlz77.o \
+./src/tinfgzip.o \
+./src/tinflate.o \
+./src/tinfzlib.o 
+
+C_DEPS += \
+./src/adler32.d \
+./src/crc32.d \
+./src/defl_static.d \
+./src/genlz77.d \
+./src/tinfgzip.d \
+./src/tinflate.d \
+./src/tinfzlib.d 
+
+
+# Each subdirectory must supply rules for building sources it contributes
+src/%.o: ../src/%.c src/subdir.mk
+	@echo 'Building file: $<'
+	@echo 'Invoking: Cross GCC Compiler'
+	/opt/gnuarmemb/bin/arm-none-eabi-gcc -std=c99 -fno-builtin -fno-omit-frame-pointer -fdata-sections -fno-exceptions -fno-builtin -fgnu89-inline -I../../include -I../mini_matrix -I../top/include -I../rstd/include -I../rstd/gram_matrix -I../rstd/host -I../hp_rstd/include -I../loc/include -I../sbe/include -I../date/include -I../../fft/inc -I../../fft/cmsis_fft/include -I../../fft/cmsis_fft/include/core -I../../loc_server/include -I../../phywi_inc -I$(ZEPHYR_BASE)/include -I$(ZEPHYR_BASE)/../nrf/include -I$(ZEPHYR_BASE)/../nrfxlib/nrf_modem/include/ -I$(ZEPHYR_BASE)/../mbedtls/include/ -Os -g -Wall -c -fmessage-length=0 -ffreestanding -fno-common -mcpu=cortex-m33 -mthumb -mabi=aapcs -fno-pie -fno-pic -fno-strict-overflow -fno-reorder-functions -fno-defer-pop -fno-asynchronous-unwind-tables -mfloat-abi=hard -MMD -MP -MF"$(@:%.o=%.d)" -MT"$@" -o "$@" "$<"
+	@echo 'Finished building: $<'
+	@echo ' '
+
+
